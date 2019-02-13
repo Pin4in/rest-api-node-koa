91e0979a51ad7cad3e5c7958088edd18f8ca15c1
pin4in <sergii.korkh@gmail.com>
2019-02-14 00:38:59 +0200

    use middleware to load user by id and error hendling

--- router.js
+++ router.js
@@ -1,24 +1,55 @@
 const Router = require('koa-router');
-const router = new Router();
-const mongoose = require('mongoose');
+const mongoose = require('./libs/mongoose');
 const pick = require('lodash/pick');
 const User = require('./models/User');
 
-module.exports = router
-  .get('/users/:id', async ctx => {
-    const { id } = ctx.params;
-    const user = await User.findById(id)
-      .catch(err => {
-        if (err.message === 'User not found' || err.name === 'CastError') {
-          return ctx.throw('User not found', 404);
-        }
-        console.log(err); // eslint-disable-line no-console
-        return ctx.throw(500);
-      });
+const router = new Router({
+  prefix: '/users'
+});
 
-    ctx.body = user;
-  })
-  .get('/users/', async ctx => {
+
+const loadById = async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.throw(404);
+  }
+
+  ctx.userById = await User.findById(id);
+
+  if (!ctx.userById) {
+    ctx.throw(404, 'user not found');
+  }
+
+  await next();
+};
+
+async function handleValidationError(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    if (err.name == 'ValidationError') {
+      ctx.status = 400;
+
+      const errors = {};
+
+      for (const field in err.errors) {
+        if (err.errors.hasOwnProperty(field)) {
+          errors[field] = err.errors[field].message;
+        }
+      }
+
+      ctx.body = {
+        errors: errors
+      };
+    } else {
+      throw err;
+    }
+  }
+}
+
+
+module.exports = router
+  .get('/', async ctx => {
     const users = await User.find()
       .catch(err => {
         console.log(err); // eslint-disable-line no-console
@@ -27,106 +58,23 @@ module.exports = router
 
     if (users.length === 0) ctx.throw('Users not found', 404);
 
-    ctx.body = users;
+    ctx.body = users.map(user => user.serialize());
   })
-  .post('/users/', async ctx => {
-    const userObj = Object.assign({}, ctx.request.body);
-    // prevent user to provide an _id field
-    delete userObj._id;
-
-    ctx.body = await User.create(userObj)
-      .then(({ _id }) => _id)
-      .catch(err => {
-        console.log(err);
-        if (err.name === 'ValidationError') {
-          ctx.status = 400;
-          return err;
-        } else {
-          ctx.throw(500);
-        }
-      });
+  .get('/:id', loadById, async ctx => {
+    ctx.body = ctx.userById.serialize();
   })
-  .patch('/users/:id', async ctx => {
-    /* eslint no-console: ["error", { allow: ["log"] }] */
-    const { id } = ctx.params;
-    const modifiedProperties = pick(ctx.request.body, ['email', 'displayName']);
-
-    // check if user exists
-    const updateUser = async () => {
-      const user = await User.findById(id)
-        .catch(err => {
-          if (err.message === 'User not found' || err.name === 'CastError') {
-            return ctx.throw('User not found', 404);
-          }
-          console.log('---find error', err); // eslint-disable-line no-console
-          return ctx.throw(500);
-        });
-      console.log('user', user);
-
-      const updatedUser = await User.updateOne({ _id: id }, modifiedProperties)
-        .then(data => {
-          if (data.nModified && data.ok) {
-            return Object.assign({}, user, modifiedProperties);
-          }
-          ctx.throw(500);
-        })
-        .catch(err => {
-          console.log('---update error', err); // eslint-disable-line no-console
-          return ctx.throw(500);
-        });
-    };
-
-    ctx.body = await updateUser()
-      .catch(err => {
-        console.log('---got an err', err);
-        if (err.name === 'CastError') {
-          return ctx.throw('User not found', 404);
-        }
-        return ctx.throw(500);
-      })
-      .then(result => {
-        console.log('--- user', result);
-        if (!result.nModified) ctx.throw('User not found', 404);
-        return 200;
-      });
-
-    // ctx.body = updatedUser;
-
-    // ctx.body = await User.updateOne({ _id: id }, userObj)
-    //   .then((err, data) => {
-    //     if (err) {
-    //       console.log('---data', err);
-    //       return err;
-    //     }
-    //     console.log('---data', data);
-    //     return data;
-    //   });
-    //   .then(({ _id }) => _id)
-    //   .catch(err => {
-    //     if (err.name === 'ValidationError') {
-    //       ctx.status = 400;
-    //       return err;
-    //     } else {
-    //       ctx.throw(500);
-    //     }
-    //   });
+  .post('/', handleValidationError, async ctx => {
+    const user = await User.create(pick(ctx.request.body, User.publicFields));
+    ctx.body = user.serialize();
   })
-  .del('/users/:id', async ctx => {
-    ctx.body = 'hello';
+  .patch('/:id', loadById, handleValidationError, async ctx => {
+    Object.assign(ctx.userById, pick(ctx.request.body, User.publicFields));
+    await ctx.userById.save();
+
+    ctx.body = ctx.userById.serialize();
   })
-  .get('/create-users/', async ctx => {
-    await User.remove({});
-    await User.create({ email: 'john@gmail.com', displayName: 'John' });
-    await User.create({ email: 'pete@gmail.com', displayName: 'Pete' });
-    await User.create({ email: 'mary@gmail.com', displayName: 'Mary' });
-    ctx.body = 'created';
+  .del('/:id', loadById, async ctx => {
+    await ctx.userById.delete();
+    ctx.body = 'Ok';
   })
 ;
-
-
-// {"message": "Cast to ObjectId failed for value \"5c58c6f4d03de2ba68ee517\" at path \"_id\" for model \"User\"",
-// "name":"CastError",
-// "stringValue":"\"5c58c6f4d03de2ba68ee517\"",
-// "kind":"ObjectId",
-// "value":"5c58c6f4d03de2ba68ee517",
-// "path":"_id"}
