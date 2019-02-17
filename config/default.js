2848ebf1547e827e8362c607406ce04c543e79f3
pin4in <sergii.korkh@gmail.com>
2019-02-17 23:09:47 +0200

    add env PORT propery

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  port: 3000,
+  port: process.env.PORT || 3000,
   secret: 'mysecret',
   root: process.cwd(),
   templatesRoot: path.join(process.cwd(), 'templates'),
